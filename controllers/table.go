825d1cc7fe14e2e3903b68104b8bcf425861a90f
cay <2020>
2021-11-05 17:49:36 +0800

    审批一半

--- controllers/table.go
+++ controllers/table.go
@@ -1,11 +1,17 @@
 package controllers
 
+import (
+	"HuiZhen/models"
+	"encoding/json"
+)
+
 type TableController struct {
 	BaseController
 }
 
 func (c *TableController) Get() {
 	if c.IsLogin {
+
 		c.Data["A1"] = "{{# if(d.JYConFormPolicy == 1){ }}"
 		c.Data["A2"] = "{{# }else{ }}"
 		c.Data["A3"] = "{{# } }}"
@@ -14,14 +20,31 @@ func (c *TableController) Get() {
 		c.Data["A5"] = "{{#  if(d.JYConOppHos == "
 		c.Data["A6"] = "){ }}\n    温江区医院\n    {{#  } else if(d.JYConOppHos == "
 		c.Data["A7"] = "){ }}\n    精医中心\n    {{# } }}"
+
+		c.Data["approveOldState"] = " {{#  if(d.JYConFormApproveOldState == 1){ }}\n        申请状态\n    {{#  } else if(d.JYConFormApproveOldState == 2){ }}\n        科室审批\n    {{#  } else if(d.JYConFormApproveOldState == 3){ }}\n        申请方医务部审批\n    {{#  } else if(d.JYConFormApproveOldState == 4){ }}\n        接收方医务部审批\n    {{#  } else if(d.JYConFormApproveOldState == 5){ }}\n         接收方科室审批\n    {{# } }}"
+		c.Data["approveState"] = "    {{#  if(d.JYConFormApproveState==\"approve\"){ }}\n    通过\n    {{#  } else if(d.JYConFormApproveState==\"reject\"){ }}\n    申请方科室审批\n    {{# } }}\n"
 		id := c.Ctx.Input.Param(":id")
 		switch id {
 		case "my":
 			c.TplName = "table.html"
 			break
 		case "from_dsp":
+			isKsfz := false
+			IsTO := false
 			Type := c.GetString("type")
 			c.Data["Type"] = Type
+			if c.IsKszr || c.IsYwb {
+				isKsfz = true
+				if Type == "to" {
+					IsTO = true
+				}
+			}
+			docList := models.GetDocList(c.PersonUer.JYConPersonBelongHos, c.PersonUer.JYConPersonBelongDep, c.PersonUer.JYConPersonCode, "200", "1")
+			res := models.Person{}
+			_ = json.Unmarshal([]byte(docList), &res)
+			c.Data["DocList"] = res.Data
+			c.Data["IsTO"] = IsTO
+			c.Data["IsKsfz"] = isKsfz
 			c.TplName = "table_from.html"
 			break
 		case "from_ysp":
